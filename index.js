e13b494c33b050463e9842a0f74df2b54ec5b259
rk-codes <ping.you.back@gmail.com>
2017-10-02 01:06:45 -0700

    add previous and next page navigation

--- index.js
+++ index.js
@@ -1,48 +1,72 @@
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+let nextPage = 0;
+let prevPage = 0;
+let query = "";
 
-function getDataFromApi(searchTerm, callback) {
-  const params = {
+function getDataFromApi(searchTerm, callback, page) {
+    const params = {
     part: 'snippet',
     key: 'AIzaSyArySOG4MTzHdufypzKbBUrdesEvEdljYs',
     q: searchTerm,
-    maxResults: 10
+    maxResults: 10,
+    pageToken: page
   }
   $.getJSON(YOUTUBE_SEARCH_URL, params, callback);
 }
 
 function renderResult(result) {
-   let imgUrl = result.snippet.thumbnails.default.url;
+   let imgUrl = result.snippet.thumbnails.medium.url;
    let videoId = result.id.videoId;
    let videoDescription = result.snippet.description;
    let title = result.snippet.title;
    let chId = result.snippet.channelId;
-   console.log("ID:" + result.id.videoId);
    return `
    <div class = "result-box">
      <a href="https://www.youtube.com/watch?v=${videoId}"><img src = "${imgUrl}" class="img-thumbnail"></a>
      <div class = "info">
         <span class="video-title">"${title}"</span>
         <p>"${videoDescription}"</p>
-        <a href="https://www.youtube.com/channel/${chId}">More from this channel</a>
+        <a href="https://www.youtube.com/channel/${chId}/videos">More from this channel</a>
      </div>
-     
     </div`
   
 }
 
 function displayYouTubeSearchData(data) {
-  console.log("Length:" +data.items.length);
-  console.log(data.items);
-   console.log(data.items[0].snippet.thumbnails.default);
    const results = data.items.map((item,index) => renderResult(item));
+   nextPage = data.nextPageToken;
+   prevPage = data.prevPageToken;
+   $('h2').text(`Results for "${query}"`);
+   $('.js-search-results').html(results);
+   $('.next-button').show();
+   if(data.prevPageToken){
+      $('.prev-button').show();
+   }
+   else{
+     $('.prev-button').hide();
+   }
    $('.js-search-results').html(results);
 }
+
+function handleNextClick(){
+  $('.next-button').click(function(event){
+    getDataFromApi(query, displayYouTubeSearchData, nextPage);
+  });
+}
+$(handleNextClick());
+
+function handlePrevClick(){
+  $('.prev-button').click(function(event){
+     getDataFromApi(query, displayYouTubeSearchData, prevPage);
+  });
+}
+$(handlePrevClick());
 
 function watchSubmit() {
   $('.js-search-form').submit(event => {
     event.preventDefault();
     const queryTarget = $(event.currentTarget).find('.js-query');
-    const query = queryTarget.val();
+    query = queryTarget.val();
     // clear out the input
     queryTarget.val("");
     getDataFromApi(query, displayYouTubeSearchData);
@@ -50,3 +74,9 @@ function watchSubmit() {
 }
 
 $(watchSubmit);
+
+function init(){
+  $('.next-button').hide();
+  $('.prev-button').hide();
+}
+$(init());
